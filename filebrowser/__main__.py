469a70bfe5037d4f157dba3d600bbe8893a0ad27
Daan van der Kallen <mail@daanvdk.com>
2025-01-06 14:17:28 -0500

    Add entrypoint

--- filebrowser/__main__.py
+++ filebrowser/__main__.py
@@ -8,7 +8,11 @@ parser.add_argument('path', nargs='?', default='.')
 parser.add_argument('-s', '--selected')
 
 
-if __name__ == '__main__':
+def main():
     args = parser.parse_args()
     app = App(args.path, args.selected)
     app.run()
+
+
+if __name__ == '__main__':
+    main()
